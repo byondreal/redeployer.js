ebbe61f3f7428b328ef414ff223a47513d97c578
ByondReal Dev <dev@byondreal.net>
2015-07-05 12:42:17 -0400

    allowing cwd to be set

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var sh = require('shelljs');
-module.exports = function (command) {
+module.exports = function (command, cwd) {
+  if (cwd) sh.cd(cwd);
   return function (req, res) {
     sh.exec('git pull');
     sh.exec('npm install --unsafe-perm');
